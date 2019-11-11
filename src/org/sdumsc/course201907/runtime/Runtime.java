5e0369dfa48471b3c6081812f95bea4aa1317bc3
26697 <26697@DESKTOP-CFAMI8E>
2019-11-11 10:51:48 +0800

    Initial Commit

---
+++ src/org/sdumsc/course201907/runtime/Runtime.java
@@ -0,0 +1,23 @@
+// ================================================================================
+//  Runtime.java
+// --------------------------------------------------------------------------------
+//  The entrance of client.
+//  Author: Lagomoro <Yongrui Wang>
+// ================================================================================
+package org.sdumsc.course201907.runtime;
+
+public class Runtime {
+	
+	public static final String VERSION = "V2.1.1(20190413-0x00EA13)";
+	public static final String AUTHOR = "Lagomoro";
+	public static final String ORGANIZATION = "山东大学齐鲁软件学院";
+	public static final String LISENCE = "Copyright (c) 2019 Lagomoro All Rights Reserved.";
+
+	public static void main(String[] args) {
+		//启动周期更新线程
+		//ThreadController.startUpdate();
+		//窗口控制器运行
+		//WindowController.run();
+	}
+	
+}
